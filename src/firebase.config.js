65ee3036b7d212b8e5ba0f8e1a683be73c10eef1
Kevin Risqi <kevinrisqi22@gmail.com>
2020-04-16 14:31:26 +0700

    Add configuration firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyAqNUb8JSdB4dsSwYbc-9vIVr966kTLH3k",
+    authDomain: "reactjs-firebase-56ae8.firebaseapp.com",
+    databaseURL: "https://reactjs-firebase-56ae8.firebaseio.com",
+    projectId: "reactjs-firebase-56ae8",
+    storageBucket: "reactjs-firebase-56ae8.appspot.com",
+    messagingSenderId: "872963743373",
+    appId: "1:872963743373:web:935166c69edb6f8ef90cc4",
+    measurementId: "G-DGQ06MZY60"
+  };
